904279dccbd387c884f48e2ce1867341de80b954
AliKazankaya <aliikazankaya@gmail.com>
2022-07-03 00:01:36 +0300

    changes are completed

--- src/test/java/SearchBoxTest.java
+++ src/test/java/SearchBoxTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.Assert;
+
 
 public class SearchBoxTest {
 
